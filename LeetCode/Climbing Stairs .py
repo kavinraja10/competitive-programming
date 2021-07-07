d1089834a7f3d29cce314ba6e4a62dbe08e6470b
errorsandbugs <65387802+errorsandbugs@users.noreply.github.com>
2021-07-07 13:34:28 +0530

    Create Climbing Stairs .py

---
+++ LeetCode/Climbing Stairs .py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        l=[1,2]
+        if n==1:
+            return 1
+        while len(l)<n:
+            #print(10)
+            
+            l.append(l[(len(l)-1)]+l[(len(l)-2)])
+        return (l[len(l)-1])    
